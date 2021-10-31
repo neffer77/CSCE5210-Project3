d4ecfdedb642653465589c5431bd7c9638614052
Connor <connor.neff77@gmail.com>
2021-10-31 11:13:35 -0500

    created board

--- Project3.py
+++ Project3.py
@@ -5,3 +5,27 @@ Created on Sun Oct 31 10:34:05 2021
 @author: conno
 """
 
+#need to make a board - lets try to be as simple as possible 2d array
+
+#need alpha beta algorithm
+
+#need user input
+
+class board:
+    def __init__(self):
+        self.board = [["","|","","|",""],["-","-","-","-"],["","|","","|",""],["-","-","-","-"],["","|","","|",""]]
+        
+                    
+    def printBoard(self):
+        for i in self.board:
+            print("")
+            for j in i:
+                print(j, end =" ")           
+    
+
+def main():
+  brd = board()
+  brd.printBoard()
+   
+if __name__ == "__main__":
+    main()
