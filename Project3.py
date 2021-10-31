107f56bc37424123b096d853027c2f631114a11c
Connor <connor.neff77@gmail.com>
2021-10-31 15:08:52 -0500

    getting minimax outline

--- Project3.py
+++ Project3.py
@@ -26,18 +26,72 @@ class board:
                 print(j, end =" ") 
                 
 class node:
-    def __init__(self,value):
+    def __init__(self,value,name):
         self.value = value
         self.left = ""
         self.right = ""
+        self.name = name
         
         
 class tree:
     def __init__(self):
+        self.root = node("")
+        
         
     def isEndState(self):
         
     def generateNodes(self):
+        
+
+class agentFunction:
+    def __init__(self):
+        self.currentNode = ""
+        self.userInput = userInput()
+        self.alphabeta = alphabeta()
+        self.tree = tree()
+        self.board = board()
+        
+    def minimax(self):
+        action = self.getmax()
+        
+        
+        
+    def dfs(self):
+        
+    def getmax(self,v):
+        v = [-1,-1]
+        if self.tree.isEndState():
+            return v
+        nodes =  self.tree.generateNodes()
+        for n in nodes:
+            getmax(v,getmin(v))
+        if v > self.alphabeta.getValue():
+            return v
+        
+    def getmin(self,v):
+        
+        if self.tree.isEndState():
+            return v;
+        input = self.userInput.getInput()
+        
+        
+
+class alphabeta:
+    def __init__(self):
+        
+
+class userInput:
+    def __init__(self):
+        self.input = ""
+        
+    def getInput(self):
+        self.input = input("Select Input Number: ")
+        return self.getInput()
+        
+class action:
+    def __init__(self):
+        
+    
         
 
 def main():
