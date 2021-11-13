c7044423166e860e9d117126542bdd3ab4fe4206
Connor <connor.neff77@gmail.com>
2021-11-13 11:56:51 -0600

    outlining minimax still need to figure out generate nodes in tree class

--- Project3.py
+++ Project3.py
@@ -13,10 +13,11 @@ Created on Sun Oct 31 10:34:05 2021
         #recursive tree generation exploring all states
 
 #need user input
+import copy
 
 class board:
-    def __init__(self):
-        self.board = [["","|","","|",""],["-","-","-","-"],["","|","","|",""],["-","-","-","-"],["","|","","|",""]]
+    def __init__(self, board = [["","|","","|",""],["-","-","-","-"],["","|","","|",""],["-","-","-","-"],["","|","","|",""]]):
+        self.board = board
         
                     
     def printBoard(self):
@@ -26,41 +27,69 @@ class board:
                 print(j, end =" ") 
                 
 class node:
-    def __init__(self,value,name):
+    def __init__(self,value,name, board):
         self.value = value
-        self.left = ""
-        self.right = ""
         self.name = name
+        self.board = board
+        self.endStateCombos = [[[0,0],[0,2],[0,4]],[[2,0],[2,2],[2,4]],[[4,0],[4,2],[4,4]],[[0,0],[2,0],[4,0]],[[0,2],[2,2],[4,2]],[[0,4],[2,4],[4,4]],[[0,0],[2,2],[4,4]],[[0,4],[2,2],[4,0]]]
         
-        
-class tree:
-    def __init__(self):
-        self.root = node("")
-        self.endStateCombos = [[[0,0],[0,3],[0,5]],[[2,0],[2,3],[2,5]],[[4,0],[4,3],[4,5]],[[0,0],[3,0],[5,0]],[[0,2],[3,2],[5,2]],[[0,4],[3,4],[5,4]],[[0,0],[2,2],[5,5]],[0,5],[2,2],[5,0]]]
-        self.board = board()    
-    
     def isEndState(self):
         value = ["X","O"]
         for k in range(2):
             for i in self.endStateCombos:
                 count = 0 
+                
                 for j in range(3):
                     coords = i[j]
-                    if self.board[coords[0],coords[1]] == value[k]:
+                    if self.board.board[coords[0]][coords[1]] == value[k]:
                         count = count +1
                 if count == 3:
                     return True
         return False
-    
-    def generateNodes(self):
         
+    
+    
 
+class tree:
+       def __init__(self,value,name, board):
+           
+           
+       def generateNodes(self, turn):
+        currentState = int(self.name)
+        spots = self.getOpenSpots()
+        nodes = []
+        b = board(self.board.board)
+        for i in spots:
+            n = node(-1,str(currentState + 1) , b)
+            n.board.board = self.addMove(n.board.board,turn, i)
+            
+            nodes.append(n)
+            currentState = currentState + 1
+        return nodes
+    
+    def getOpenSpots(self):
+        spots = []
+        for i in [0,2,4]:
+            for j in range(5):
+                if (self.board.board[i][j] == ""):
+                    spots.append([i,j])
+        return spots
+     
+    def addMove(self,board,turn, spot):
+        if turn == 'X':
+            board[spot[0]][spot[1]] = 'X'
+        elif turn == 'O':
+            board[spot[0]][spot[1]] = 'O'
+        else:
+            print("Error: addMove")
+        return board
 class agentFunction:
     def __init__(self):
-        self.currentNode = node("","1")
         self.userInput = userInput()
         self.alphabeta = alphabeta()
-        self.tree = tree()
+        self.currentNode = node(-1,"1",board())
+        self.alpha = "inf"
+        self.beta = "-inf"
         
     def minimax(self):
         n = self.getmax(self.currentNode)
@@ -69,20 +98,24 @@ class agentFunction:
         
         
     def getmax(self,n):
-        if self.tree.isEndState():
+        if n.isEndState():
             return n
-        nodes =  self.tree.generateNodes()
+        nodes =  n.generateNodes('X')
         for n in nodes:
-            getmax(n,getmin(n))
-        if v > self.alphabeta.getValue():
-            return n
+            self.getmax(n,self.getmin(n))
+       # if self.alpha > self.alphabeta.getValue():
+        #    return n
         
     def getmin(self,n):
         
-        if self.tree.isEndState():
-            return state;
-        input = self.userInput.getInput()
-        return state
+        if n.isEndState():
+            return n;
+        nodes = n.generateNodes('O')
+        for n in nodes:
+            self.getmin(n,self.getmax(n))
+            
+        #if self.beta < self.alphabeta.getValue():
+         #   return n
         
         
 
@@ -94,7 +127,10 @@ class alphabeta:
         return self.beta
     
     def determineBeta():
+        print("a")
         
+    def determineAlpha():
+        print("a")
         
 
 class userInput:
@@ -107,13 +143,14 @@ class userInput:
         
 class action:
     def __init__(self):
-        
+        print("a")
     
         
-
 def main():
   brd = board()
   brd.printBoard()
+  agtFunc = agentFunction()
+  agtFunc.minimax()
    
 if __name__ == "__main__":
     main()
